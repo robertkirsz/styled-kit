7cdef8846333c39693acf8cd6179f7189dc8da95
Robert Kirsz <robert.kirsz@gmail.com>
2018-11-27 09:52:37 +0100

    Update Babel config

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,15 @@
-module.exports = () => ({
-  presets: ['@babel/env', '@babel/react'],
-  plugins: [
+module.exports = api => {
+  // I'm not sure why I need that but Jest screams about it
+  api.cache.forever()
+
+  const presets = ['@babel/env', '@babel/react']
+
+  const plugins = [
     '@babel/proposal-export-default-from',
     '@babel/proposal-class-properties',
     '@babel/proposal-optional-chaining',
     'styled-components'
   ]
-})
+
+  return { presets, plugins }
+}
