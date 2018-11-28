e9f5b3a297b1fa608031b469018347a72109aabb
Robert Kirsz <robert.kirsz@gmail.com>
2018-11-28 01:20:07 +0100

    Update ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: 'react-app',
+  plugins: ['babel'],
   rules: {
-    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }]
+    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
+    "no-unused-expressions": "off", // This doesn't work with '?.' operator syntax, so we disable it...
+    "babel/no-unused-expressions": ["error", { allowShortCircuit: true, allowTernary: true }], // ...and enable this one instead, which is a modern version of the former
   }
 }
